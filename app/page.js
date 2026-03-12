634fa77883b2a7dfe924a19f7198916f0e71f59a
JWinegar06 <winegaj86@gmail.com>
2026-03-12 16:49:21 -0600

    Expense Modal Setup

--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
 "use client";
 
+import { useState, useRef, useEffect } from "react";
+
 import { currencyFormatter } from "@/lib/utils";
 
 import ExpenseCategoryItem from "@/components/ExpenseCategoryItem";
+import Modal from "@/components/Modal"
 
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
@@ -37,53 +40,70 @@ const DUMMY_DATA = [
 ];
 
 export default function Home() {
+  const [modalIsOpen, setModalIsOpen] = useState(true);
+
   return (
-    <main className="container max-w-2xl px-6 mx-auto">
-      <section className="py-3">
-        <small className="text-white text-md">My Balance</small>
-        <h2 className="text-4xl font-bold">{currencyFormatter(100000)}</h2>
-      </section>
+    <>
+      {/* Modal */}
 
-      <section className="flex items-center gap-2 py-3">
-        <button className="btn btn-primary">+ Expenses</button>
-        <button className="btn btn-primary-outline">+ Income</button>
-      </section>
+      <Modal show={modalIsOpen} onClose={setModalIsOpen}>
+        <h3>Hello</h3>
+      </Modal>
 
-      {/* Expense Section */}
-      <section className="py-6">
-        <h3 className="text-2xl">My Expenses</h3>
-        <div className="flex flex-col gap-4 mt-6">
-          {DUMMY_DATA.map((expense) => (
-            <ExpenseCategoryItem
-              key={expense.id}
-              color={expense.color}
-              title={expense.title}
-              total={expense.total}
-            />
-          ))}
-        </div>
-      </section>
+      <main className="container max-w-2xl px-6 mx-auto">
+        <section className="py-3">
+          <small className="text-white text-md">My Balance</small>
+          <h2 className="text-4xl font-bold">{currencyFormatter(100000)}</h2>
+        </section>
 
-      {/* Stats Chart */}
-      <section className="py-6">
-        <h3 className="text-2xl">Stats</h3>
-        <div className="w-1/2 mx-auto">
-          <Doughnut
-            data={{
-              labels: DUMMY_DATA.map((expense) => expense.title),
-              datasets: [
-                {
-                  label: "Expenses",
-                  data: DUMMY_DATA.map((expense) => expense.total),
-                  backgroundColor: DUMMY_DATA.map((expense) => expense.color),
-                  borderColor: ["black"],
-                  borderWidth: 2,
-                },
-              ],
+        <section className="flex items-center gap-2 py-3">
+          <button
+            onClick={() => {
+              setModalIsOpen(true);
             }}
-          />
-        </div>
-      </section>
-    </main>
+            className="btn btn-primary"
+          >
+            + Expenses
+          </button>
+          <button className="btn btn-primary-outline">+ Income</button>
+        </section>
+
+        {/* Expense Section */}
+        <section className="py-6">
+          <h3 className="text-2xl">My Expenses</h3>
+          <div className="flex flex-col gap-4 mt-6">
+            {DUMMY_DATA.map((expense) => (
+              <ExpenseCategoryItem
+                key={expense.id}
+                color={expense.color}
+                title={expense.title}
+                total={expense.total}
+              />
+            ))}
+          </div>
+        </section>
+
+        {/* Stats Chart */}
+        <section className="py-6">
+          <h3 className="text-2xl">Stats</h3>
+          <div className="w-1/2 mx-auto">
+            <Doughnut
+              data={{
+                labels: DUMMY_DATA.map((expense) => expense.title),
+                datasets: [
+                  {
+                    label: "Expenses",
+                    data: DUMMY_DATA.map((expense) => expense.total),
+                    backgroundColor: DUMMY_DATA.map((expense) => expense.color),
+                    borderColor: ["black"],
+                    borderWidth: 2,
+                  },
+                ],
+              }}
+            />
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
